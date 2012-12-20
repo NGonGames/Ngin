3d780830baf1363581096a58620eb481e30c9724
Ben Cochrane <ben@ngon.cc>
2012-12-20 15:39:18 -0500

    Commited makefile variables

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,43 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug_Windows configuration
+CND_PLATFORM_Debug_Windows=MinGW-Windows
+CND_ARTIFACT_DIR_Debug_Windows=dist/Debug_Windows/MinGW-Windows
+CND_ARTIFACT_NAME_Debug_Windows=ngin
+CND_ARTIFACT_PATH_Debug_Windows=dist/Debug_Windows/MinGW-Windows/ngin
+CND_PACKAGE_DIR_Debug_Windows=dist/Debug_Windows/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug_Windows=ngin.tar
+CND_PACKAGE_PATH_Debug_Windows=dist/Debug_Windows/MinGW-Windows/package/ngin.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=ngin
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/ngin
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=ngin.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/ngin.tar
+# Debug_Linux configuration
+CND_PLATFORM_Debug_Linux=MinGW-Windows
+CND_ARTIFACT_DIR_Debug_Linux=dist/Debug_Linux/MinGW-Windows
+CND_ARTIFACT_NAME_Debug_Linux=ngin
+CND_ARTIFACT_PATH_Debug_Linux=dist/Debug_Linux/MinGW-Windows/ngin
+CND_PACKAGE_DIR_Debug_Linux=dist/Debug_Linux/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug_Linux=ngin.tar
+CND_PACKAGE_PATH_Debug_Linux=dist/Debug_Linux/MinGW-Windows/package/ngin.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
