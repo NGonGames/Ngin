a998bbb905985160c03b3a2ea28e3a06c598ba82
Ben Cochrane <syynth@syynth.com>
2013-01-14 01:05:02 -0500

    Added netbeans project to the projects/netbeans directory.

---
+++ projects/netbeans/nbproject/Makefile-variables.mk
@@ -0,0 +1,43 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug_Windows configuration
+CND_PLATFORM_Debug_Windows=MinGW-Windows
+CND_ARTIFACT_DIR_Debug_Windows=dist/Debug_Windows/MinGW-Windows
+CND_ARTIFACT_NAME_Debug_Windows=netbeans
+CND_ARTIFACT_PATH_Debug_Windows=dist/Debug_Windows/MinGW-Windows/netbeans
+CND_PACKAGE_DIR_Debug_Windows=dist/Debug_Windows/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug_Windows=netbeans.tar
+CND_PACKAGE_PATH_Debug_Windows=dist/Debug_Windows/MinGW-Windows/package/netbeans.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=netbeans
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/netbeans
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=netbeans.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/netbeans.tar
+# Debug_Linux configuration
+CND_PLATFORM_Debug_Linux=MinGW-Windows
+CND_ARTIFACT_DIR_Debug_Linux=dist/Debug_Linux/MinGW-Windows
+CND_ARTIFACT_NAME_Debug_Linux=netbeans
+CND_ARTIFACT_PATH_Debug_Linux=dist/Debug_Linux/MinGW-Windows/netbeans
+CND_PACKAGE_DIR_Debug_Linux=dist/Debug_Linux/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug_Linux=netbeans.tar
+CND_PACKAGE_PATH_Debug_Linux=dist/Debug_Linux/MinGW-Windows/package/netbeans.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
